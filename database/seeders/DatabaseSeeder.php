322fab8f2063fecda8f66bbb752805e528ae0c9b
Kemz <afilfriend@gmail.com>
2024-08-02 13:36:30 +0800

    feat: add required seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\SocialMedia;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Charissa Florist Admin',
+            'email' => 'charissa-florist@gmail.com',
+            'password' => bcrypt('bMj26rcsKDSlaiw7B44H'),
         ]);
+
+        $socialMedia = [
+            [
+                'name' => 'Instagram',
+                'url' => 'https://www.instagram.com/charissaflorist/'
+            ],
+            [
+                'name' => 'TikTok',
+                'url' => 'https://www.tiktok.com/@charissaflorist'
+            ],
+        ];
+
+        foreach ($socialMedia as $socmed) {
+            SocialMedia::create($socmed);
+        }
     }
 }
