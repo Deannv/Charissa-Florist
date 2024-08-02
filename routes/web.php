8e01363e8df153a064043c1e012d6a8ec8111af9
Kemz <afilfriend@gmail.com>
2024-08-02 13:36:43 +0800

    feat: add new route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Models\SocialMedia;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::post('/send', [HomeController::class, 'send'])->name('send');
